3ab406b602bd634379c410c810606704baeab769
Kees van Ekeren <kees.van.ekeren@mobpro.com>
2020-09-22 21:38:24 +0200

    config

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,26 @@
  */
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: "TROEPTROEP"
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-remark`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/typography`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/data/`
+      },
+    },
+  ],
 }
