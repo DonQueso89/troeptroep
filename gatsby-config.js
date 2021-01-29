cc9e7694c3ed983a34ab8c12ba969a964c1371fc
DonQueso89 <kg.v.ekeren@gmail.com>
2021-01-29 09:18:55 +0100

    createResolvers attemtp

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,32 @@
-require("dotenv").config()
-
+require("dotenv").config();
 
 module.exports = {
   siteMetadata: {
-    title: "TROEPTROEP"
+    title: "TROEPTROEP",
   },
   plugins: [
+    {
+      resolve: `gatsby-source-datocms`,
+      options: {
+        apiToken: process.env.GATSBY_DATOCMS_READ_KEY,
+
+        // If you are working on development/staging environment, you might want to
+        // preview the latest version of records instead of the published one:
+        previewMode: false,
+
+        // Disable automatic reloading of content when some change occurs on DatoCMS:
+        disableLiveReload: true,
+
+        // Custom API base URL (most don't need this)
+        // apiUrl: 'https://site-api.datocms.com',
+
+        // Setup locale fallbacks
+        // In this example, if some field value is missing in Italian, fall back to English
+        //localeFallbacks: {
+        //  it: ["en"],
+        //},
+      },
+    },
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
@@ -14,8 +35,8 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `data`,
-        path: `${__dirname}/src/data/`
+        path: `${__dirname}/src/data/`,
       },
     },
   ],
-}
+};
