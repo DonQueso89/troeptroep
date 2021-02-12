e512f9a7608b3d8d64f3b38e2987771b035fb7c6
DonQueso89 <kg.v.ekeren@gmail.com>
2021-02-12 18:39:49 +0100

    rm obsolete query

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,72 +1,48 @@
-import React, { useEffect } from "react"
-import { graphql } from "gatsby"
-import Container from "../components/container"
-import MainLogo from "../assets/TroepTroep.jpg"
-import galleryStyles from "../styles/gallery.module.css"
-import Img from "gatsby-image"
-import { FaBalanceScale } from "react-icons/fa"
-import HeaderVideo from "../assets/headervid.mp4"
+import React, { useEffect } from "react";
+import { graphql } from "gatsby";
+import Container from "../components/container";
+import MainLogo from "../assets/TroepTroep.jpg";
+import galleryStyles from "../styles/gallery.module.css";
+import Img from "gatsby-image";
+import { FaBalanceScale } from "react-icons/fa";
+import HeaderVideo from "../assets/headervid.mp4";
 
 const usePlayed = () => {
-  return typeof window !== 'undefined' && window.played
-}
+  return typeof window !== "undefined" && window.played;
+};
 
-export default function Home({ data }) {
+export default function Home() {
   const onEndHeaderVideo = (e) => {
     e.target.style.opacity = 0;
     document.getElementById("main-logo-img").style.opacity = 1;
-  }
-  const played = usePlayed()
+  };
+  const played = usePlayed();
 
   useEffect(() => {
-    window.played = true
+    window.played = true;
   }, []);
 
   return (
     <Container>
       <div className={galleryStyles.mainLogoContainer}>
-        { !played &&
-        <video autoPlay muted className={galleryStyles.headerVideo} onEnded={onEndHeaderVideo}>
-          <source src={HeaderVideo} type="video/mp4" />
-        </video>
-        }
-        <img className={galleryStyles.mainLogo} src={MainLogo} alt="" id="main-logo-img" data-video-ended={played} />
+        {!played && (
+          <video
+            autoPlay
+            muted
+            className={galleryStyles.headerVideo}
+            onEnded={onEndHeaderVideo}
+          >
+            <source src={HeaderVideo} type="video/mp4" />
+          </video>
+        )}
+        <img
+          className={galleryStyles.mainLogo}
+          src={MainLogo}
+          alt=""
+          id="main-logo-img"
+          data-video-ended={played}
+        />
       </div>
     </Container>
-  )
+  );
 }
-
-export const query = graphql`query {
-  allMarkdownRemark {
-    edges {
-      node {
-        frontmatter {
-          after_image {
-            childImageSharp {
-              fixed(width: 250, height: 250) {
-                ...GatsbyImageSharpFixed
-              }
-            }
-          }
-        	before_image {
-            childImageSharp {
-              fixed(width: 250, height: 250) {
-                ...GatsbyImageSharpFixed
-              }
-            }
-          }
-        	image {
-            childImageSharp {
-              fixed(width: 250, height: 250) {
-                ...GatsbyImageSharpFixed
-              }
-            }
-          }
-          date
-          latlong
-          entry_type
-        }
-      }
-    }
-  }
-}`
