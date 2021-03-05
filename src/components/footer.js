3fce7981559c420a02ff7a809000de6a528aee65
DonQueso89 <kg.v.ekeren@gmail.com>
2021-03-05 14:56:07 +0100

    quickfix

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ import containerStyles from "./container.module.css"
 const Footer = (props) => (
     <footer className={containerStyles.footer}>
         <p className="copyright">
-            ||||| |||| <Link activeStyle={{ color: "white" }} to="/manifest/" >Manifest </Link>
+            ||||| |||| <Link activeStyle={{ color: "white" }} to="/" >Manifest </Link>
              |||| |||| &copy; Built by: <a  target="_blank" rel="noreferrer" href="https://github.com/DonQueso89">DonQueso89</a> |||| ||||| </p>
     </footer>
 )
